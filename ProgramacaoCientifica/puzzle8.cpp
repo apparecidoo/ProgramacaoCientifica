498e33265aa61ca931b5d92e52280a28fff532ee
Gustavo Aparecido de Souza Viana <gustavo-aparecido.viana@scania.com>
2019-07-25 19:57:39 -0300

    create class puzzle8

--- ProgramacaoCientifica/puzzle8.cpp
+++ ProgramacaoCientifica/puzzle8.cpp
@@ -13,3 +13,43 @@ bool Puzzle8::compare(int first[SIZE][SIZE], int second[SIZE][SIZE])
 {
 	return false;
 }
+
+void Puzzle8::create_children_nodes(TreeNode<int[SIZE][SIZE]>* node)
+{
+	bool search_zero = false;
+	int i = 0;
+	int j = 0;
+
+	if (node == NULL)
+		throw CustomException("create_children_nodes - node is null or empty");
+
+	for (i = 0; i < SIZE; i++)
+	{
+		for (j = 0; j < SIZE; j++)
+		{
+			if (node->content[i][j] == 0) {
+				search_zero = true;
+				break;
+			}
+		}
+
+		if (search_zero)
+			break;
+	}
+
+	// 4 possibilities
+	if (i == 1 && j == 1) {
+		//node->children_nodes->addLast();
+		//TreeNode<int[SIZE][SIZE]>* first = new TreeNode<int[SIZE][SIZE]>();
+	}
+	else {
+		// 3 possibilites
+		if ((i == 0 && j == 1) || (i == 1 && j == 0) || (i == 2 && j == 1) || (i == 1 && j == 2)) {
+			
+		}
+		else {
+			// 2 possibilities
+
+		}
+	}
+}
